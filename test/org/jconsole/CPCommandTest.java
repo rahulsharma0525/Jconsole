036850c322f15ea9031dc630a739c6cd5a3fa57f
rahulsharma0525 <rahulsharma0525@gmail.com>
2014-03-17 17:47:23 -0500

    Second Phase of Test Cases

--- test/org/jconsole/CPCommandTest.java
+++ test/org/jconsole/CPCommandTest.java
@@ -2,7 +2,11 @@ package org.jconsole;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.PrintStream;
+
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,13 +14,23 @@ import org.junit.Test;
 public class CPCommandTest {
 
 	static CPCommand CpCom = new CPCommand();
+	static CDCommand cdCom = new CDCommand();
+	static LSCommand LSCom = new LSCommand();
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+
 	static String curDir = System.getProperty("user.dir");
+	JConsole jcon;
 	
 	@Before
 	public void setup(){
-		JConsole jcon=JConsole.instance();
+		 jcon=JConsole.instance();
 		jcon.setCurrentDir(curDir);
 		CpCom.setConsole(jcon);
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+
 	}
 	
 	@Test
@@ -69,5 +83,45 @@ public class CPCommandTest {
 			}catch (CommandFailedException e) {
 			e.printStackTrace();
 		}
+			
+				
 	}
+		
+		@Test
+		public void CopyLSDir()
+		{
+			try {
+				String srcDir = curDir + "/testResource/testDoc.txt";
+				String destDir="C:\\Users\\Rahul\\Desktop\\Zillow";
+				String [] args1 = {srcDir,destDir };
+				CpCom.execute(args1);
+				
+				//changing the Directory to Zillow
+				
+				String cdcur="C:\\Users\\Rahul\\Desktop\\Zillow";
+				jcon.setCurrentDir(cdcur);
+				cdCom.setConsole(jcon);
+				
+				String [] args2 = {cdcur};
+				cdCom.execute(args2);
+				File newFileDir = cdCom.getCurrentDir();
+				String newDir = newFileDir.toString(); 
+				
+				//List all the files
+				String [] args3 = new String[0];
+
+				jcon.setCurrentDir(cdcur);
+				LSCom.setConsole(jcon);
+				LSCom.execute(args3);
+				boolean condition=false;
+				String output=outContent.toString();
+				if(output.contains("testDoc.txt"))
+					condition=true;
+				
+				Assert.assertTrue(condition);
+				}catch (CommandFailedException e) {
+				e.printStackTrace();
+				fail("Exception was thrown");
+			}
+		}
 }
--- test/org/jconsole/PWDCommandTest.java
+++ test/org/jconsole/PWDCommandTest.java
@@ -3,6 +3,7 @@ package org.jconsole;
 import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 
@@ -14,15 +15,19 @@ import org.junit.Test;
 public class PWDCommandTest {
 
 	static PWDCommand pcom = new PWDCommand();
+	static CDCommand cdCom = new CDCommand();
+
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 	static String myDir = System.getProperty("user.dir");
+	static String curDir = System.getProperty("user.dir");
+	JConsole jcon;
 	
 	@Before
 	public void setup(){
 		System.setOut(new PrintStream(outContent));
 		System.setErr(new PrintStream(errContent));
-		JConsole jcon=JConsole.instance();
+		 jcon=JConsole.instance();
 		jcon.setCurrentDir(myDir);
 		pcom.setConsole(jcon);
 	}
@@ -41,4 +46,35 @@ public class PWDCommandTest {
 		}
 		outContent.reset();
 	}
+	
+//Added By Rahul	
+	@Test
+	public void CDCurDir()
+	{
+		try {
+			String [] args1=null;
+			pcom.execute(args1);
+			String output=outContent.toString();
+			Assert.assertTrue(output.contains(myDir));
+			String cdcur="C:\\Users\\Rahul\\Desktop\\MSC Proj\\Acceptance\\JConsole\\src";
+			jcon.setCurrentDir(curDir);
+			cdCom.setConsole(jcon);
+			
+			String [] args2 = {"src"};
+			cdCom.execute(args2);
+			File newFileDir = cdCom.getCurrentDir();
+			String newDir = newFileDir.toString(); 
+			boolean cond=cdcur.equals(newDir);
+			Assert.assertTrue(cond);
+			
+			
+			
+			} catch (CommandFailedException e) {
+			e.printStackTrace();
+			fail("Exception was thrown");
+		}
+		outContent.reset();
+	}
+	//End of addition by Rahul
+	
 }
